43942ac830ae26192b55e69ae514d71800674740
VixWizzer <175077660+V1xW1zz3r@users.noreply.github.com>
2026-07-16 14:53:39 +0800

    add owner scenario

---
+++ owner_scenario.ps1
@@ -0,0 +1,44 @@
+$starttime = Get-Date
+Write-Host -ForegroundColor Green "Deployment Started $starttime"
+
+## Create scenario users
+import-module AzureAD.Standard.Preview
+AzureAD.Standard.Preview\Connect-AzureAD -Identity -TenantID $env:ACC_TID
+$upnsuffix=$(az ad signed-in-user show --query userPrincipalName --output tsv | sed 's/.*@//')
+$password = Read-Host "Please enter a password"
+$securepassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+$gauser = "globaladminuser@$upnsuffix"
+$owneruser = "owneruser@$upnsuffix"
+$gadisplayname=$(echo $gauser | sed 's/@.*//')
+$ownerdisplayname=$(echo $owneruser | sed 's/@.*//')
+Write-Host -ForegroundColor Green "################################################################################################################"
+Write-Host -ForegroundColor Green "# Creating new admin users $gauser and $owneruser in Azure AD #"
+Write-Host -ForegroundColor Green "################################################################################################################"
+New-AzADUser -DisplayName $gadisplayname -UserPrincipalName $gauser -Password $securepassword -MailNickname $gadisplayname
+New-AzADUser -DisplayName $ownerdisplayname -UserPrincipalName $owneruser -Password $securepassword -MailNickname $ownerdisplayname
+
+## assign global administrator role to user
+$gauserid=$(az ad user list --upn $gauser --query [].id -o tsv)
+$globaladminid=$((Get-AzureADDirectoryRole | where {$_.DisplayName -eq 'Global Administrator'}).ObjectId)
+Add-AzureADDirectoryRoleMember -ObjectId $globaladminid -RefObjectId $gauserid
+
+## assign role in Azure subscription
+$subid=$(az account show --query id --output tsv)
+Write-Host -ForegroundColor Green "#########################################################################"
+Write-Host -ForegroundColor Green "# Assigning the Owner role to $owneruser #"
+Write-Host -ForegroundColor Green "#########################################################################"
+az role assignment create --role "Owner" --assignee $owneruser --scope /subscriptions/$subid
+
+## Script Output
+Start-Transcript -Path owner-scenario-output.txt
+Write-Host -ForegroundColor Green "#################################"
+Write-Host -ForegroundColor Green "# Script Output #"
+Write-Host -ForegroundColor Green "#################################"
+Write-Host -ForegroundColor Green "Azure Global Admin User:" $gauser 
+Write-Host -ForegroundColor Green "Azure Global Admin User Password:" $password
+Write-Host -ForegroundColor Green "Azure Owner Admin User:" $owneruser
+Write-Host -ForegroundColor Green "Azure Owner Admin User Password:" $password
+Write-Host -ForegroundColor Green " "
+Stop-Transcript
+$endtime = Get-Date
+Write-Host -ForegroundColor Green "Deployment Ended $endtime"
